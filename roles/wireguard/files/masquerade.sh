9727f8019fb64295f8e75bedc25b5161b05afd3e
lu6fer <lu6fer2604@gmail.com>
2023-10-24 17:48:13 +0200

    fixes

--- roles/wireguard/files/masquerade.sh
+++ roles/wireguard/files/masquerade.sh
@@ -1,20 +1,20 @@
 #!/bin/bash 
 
-NET="ens2"
+NET="enp4s0"
 VPN="wg0"
 VPN_IP="10.60.0.0/24"
 
 start() {
     sysctl -w net.ipv4.ip_forward=1
     sysctl -w net.ipv6.conf.all.forwarding=1
-        iptables -t nat -A POSTROUTING -s ${VPN_IP} -o ${NET} -j MASQUERADE
-        iptables -t nat -A POSTROUTING -o ${NET} -j MASQUERADE
-        iptables -A FORWARD -i ${VPN} -j ACCEPT
+    iptables -t nat -A POSTROUTING -s ${VPN_IP} -o ${NET} -j MASQUERADE
+    iptables -t nat -A POSTROUTING -o ${NET} -j MASQUERADE
+    iptables -A FORWARD -i ${VPN} -j ACCEPT
     iptables -A FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate NEW -j ACCEPT
     iptables -A FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     iptables -A FORWARD -i ${NET} -o ${VPN} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-        ip6tables -t nat -A POSTROUTING -o ${NET} -j MASQUERADE
-        ip6tables -A FORWARD -i ${VPN} -j ACCEPT
+    ip6tables -t nat -A POSTROUTING -o ${NET} -j MASQUERADE
+    ip6tables -A FORWARD -i ${VPN} -j ACCEPT
     ip6tables -A FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate NEW -j ACCEPT
     ip6tables -A FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     ip6tables -A FORWARD -i ${NET} -o ${VPN} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
@@ -29,8 +29,8 @@ stop() {
     iptables -D FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate NEW -j ACCEPT
     iptables -D FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     iptables -D FORWARD -i ${NET} -o ${VPN} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-        ip6tables -t nat -D POSTROUTING -o ${NET} -j MASQUERADE
-        ip6tables -D FORWARD -i ${VPN} -j ACCEPT
+    ip6tables -t nat -D POSTROUTING -o ${NET} -j MASQUERADE
+    ip6tables -D FORWARD -i ${VPN} -j ACCEPT
     ip6tables -D FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate NEW -j ACCEPT
     ip6tables -D FORWARD -i ${VPN} -o ${NET} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     ip6tables -D FORWARD -i ${NET} -o ${VPN} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
